177f11acba519adba69c77ffd96c9fd0dd2f2a36
Nico Godoy <nicogodoyalmeida@outlook.com>
2022-04-05 14:30:40 -0300

    Tela de telalhes que eu fiz achando que era loja

---
+++ BookDB-tvOS/Store/Controller/StoreViewController.swift
@@ -0,0 +1,22 @@
+//
+//  StoreViewController.swift
+//  BookDB-tvOS
+//
+//  Created by Nicolas Godoy on 04/04/22.
+//
+
+import UIKit
+import TVUIKit
+
+class StoreViewController: UIViewController {
+    
+    @IBOutlet weak var buyButton: UIButton!
+    @IBOutlet weak var readListButton: UIButton!
+    @IBOutlet weak var sampleButton: UIButton!
+    @IBOutlet weak var bookImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
