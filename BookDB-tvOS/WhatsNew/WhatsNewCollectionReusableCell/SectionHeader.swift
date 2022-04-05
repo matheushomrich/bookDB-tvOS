5eaefbd2390d27208bdb341c270e48a4934663a4
Matheus Homrich <37409567+matheushomrich@users.noreply.github.com>
2022-04-05 16:36:15 -0300

    wip - books and news data are not showing yet

---
+++ BookDB-tvOS/WhatsNew/WhatsNewCollectionReusableCell/SectionHeader.swift
@@ -0,0 +1,13 @@
+//
+//  SectionHeader.swift
+//  BookDB-tvOS
+//
+//  Created by Matheus Homrich on 05/04/22.
+//
+
+import Foundation
+import UIKit
+
+class SectionHeader: UICollectionReusableView {
+    @IBOutlet weak var sectionHeaderlabel: UILabel!
+}
