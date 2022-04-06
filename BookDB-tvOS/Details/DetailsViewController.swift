cd78d8d67bb8c849ce9fd9c216ad16222ea364a1
enzodegrazia <81195943+enzodegrazia@users.noreply.github.com>
2022-04-06 15:55:22 -0300

    Outlets&VC

--- BookDB-tvOS/Details/DetailsViewController.swift
+++ BookDB-tvOS/Details/DetailsViewController.swift
@@ -21,15 +21,62 @@ class DetailsViewController: UIViewController {
     @IBOutlet weak var star3: UIImageView!
     @IBOutlet weak var star4: UIImageView!
     @IBOutlet weak var star5: UIImageView!
+    @IBOutlet weak var buyBtn: UIButton!
+    @IBOutlet weak var listBtn: UIButton!
+    @IBOutlet weak var previewBtn: UIButton!
+    @IBOutlet var buyView: UIView!
+    @IBOutlet var listView: UIView!
+    @IBOutlet var previewView: UIView!
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
-    
-
+        setButtons()
+        setLabels()
+        
+        
         // Do any additional setup after loading the view.
     }
-
+    
+    func setLabels() {
+        bookTitleLabel.text = "Steve Jobs"
+        authorLabel.text = "Walter Isaacson"
+        descriptionLabel.text = "O livro, baseado em mais de quarenta entrevistas com Jobs ao longo de dois anos - e entrevistas com mais de cem familiares, amigos, colegas, adversários e concorrentes -, narra a vida atribulada do empresário extremamente inventivo e de personalidade forte e polêmica, cuja paixão pela perfeição e cuja energia indomável revolucionaram seis grandes indústrias: a computação pessoal, o cinema de animação, a música, a telefonia celular, a computação em tablet e a edição digital."
+        descriptionTitleLabel.text = "Description"
+        reviewLabel.text = "60 Reviews"
+    }
+    
+    func setButtons() {
+        //variaveis temporarias
+        let price = "49,90"
+        var isOwned = false
+        
+        if isOwned == true {
+            buyBtn.setTitle("Owned", for: .normal)
+            buyView.layer.backgroundColor = UIColor.gray.cgColor
+            buyView.layer.borderColor = UIColor.gray.cgColor
+        } else {
+            buyBtn.layer.cornerRadius = 10
+            buyBtn.clipsToBounds = true
+            buyBtn.setTitle("Buy | R$\(price)", for: .normal)
+            buyView.layer.backgroundColor = UIColor.orange.cgColor
+            buyView.layer.borderColor = UIColor.orange.cgColor
+            buyView.layer.borderWidth = 10
+        }
+        
+        listBtn.layer.cornerRadius = 10
+        listBtn.clipsToBounds = true
+        listBtn.setTitle("Add to list", for: .normal)
+        buyView.layer.backgroundColor = .none
+        buyView.layer.borderColor = UIColor.orange.cgColor
+        buyView.layer.borderWidth = 10
+        
+        previewBtn.layer.cornerRadius = 10
+        previewBtn.clipsToBounds = true
+        previewBtn.setTitle("Preview", for: .normal)
+        buyView.layer.backgroundColor = .none
+        buyView.layer.borderColor = UIColor.orange.cgColor
+        buyView.layer.borderWidth = 10
+    }
 
     /*
     // MARK: - Navigation
